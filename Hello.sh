9e2ee8a119a5f65817f62367bd336c26ae6c9b8e
sudinakeerthana <46190765+sudinakeerthana@users.noreply.github.com>
2021-07-28 15:11:29 -0500

    add hello.sh to brnach

--- Hello.sh
+++ Hello.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
 
 echo "what are your top 3 favourite movies :"
-read movie1 movie2 movie3
-echo "Oh! your favourite movies are: $movie1 $movie2 $movie3"
+read movie1 
+read movie2
+read movie3
+echo -e "Oh! your favourite movies are:\n$movie1\n$movie2\n$movie3"
 
